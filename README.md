1b40b7631276a75835bae27a212f7104b2abbc9d
Thomas Cashman <thomascashman404@gmail.com>
2016-05-19 19:14:31 +0200

    Refactored project for mini2Dx
    Added gradle build
    Refactored packages to org.mini2Dx
    Imported commons-beanutils and commons-collections under
    org.mini2Dx.beanutils and org.mini2Dx.collections

---
+++ README.md
@@ -0,0 +1,14 @@
+# android-beans
+
+Forked from openbeans.
+
+android-beans is a redistribution of the java.beans package from the Apache Harmony project with additional classes from commons-beanutils and commons-collections to support bean introspection and cloning. The following renaming has occurred:
+ * java.beans -> org.mini2Dx.android.beans
+ * commons-beanutils -> org.mini2Dx.beanutils
+ * commons-collections -> org.mini2Dx.collections
+
+This renaming was done to support the Android environment which does not include java.beans in it's core libraries.
+
+## License
+
+The project is licensed under the Apache License Version 2.0.
---
+++ src/main/java/org/mini2Dx/android/beans/beancontext/BeanContextServices.java
@@ -0,0 +1,57 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mini2Dx.android.beans.beancontext;
+
+import java.util.Iterator;
+import java.util.TooManyListenersException;
+
+import org.mini2Dx.android.beans.beancontext.BeanContext;
+import org.mini2Dx.android.beans.beancontext.BeanContextChild;
+import org.mini2Dx.android.beans.beancontext.BeanContextServiceProvider;
+import org.mini2Dx.android.beans.beancontext.BeanContextServiceRevokedListener;
+import org.mini2Dx.android.beans.beancontext.BeanContextServicesListener;
+@SuppressWarnings("unchecked")
+public interface BeanContextServices extends BeanContext,
+        BeanContextServicesListener {
+
+    public void addBeanContextServicesListener(BeanContextServicesListener bcsl);
+
+    public boolean addService(Class serviceClass,
+            BeanContextServiceProvider serviceProvider);
+
+    public Iterator getCurrentServiceClasses();
+
+    public Iterator getCurrentServiceSelectors(Class serviceClass);
+
+    public Object getService(BeanContextChild child, Object requestor,
+            Class serviceClass, Object serviceSelector,
+            BeanContextServiceRevokedListener bcsrl)
+            throws TooManyListenersException;
+
+    public boolean hasService(Class serviceClass);
+
+    public void releaseService(BeanContextChild child, Object requestor,
+            Object service);
+
+    public void removeBeanContextServicesListener(
+            BeanContextServicesListener bcsl);
+
+    public void revokeService(Class serviceClass,
+            BeanContextServiceProvider serviceProvider,
+            boolean revokeCurrentServicesNow);
+}
